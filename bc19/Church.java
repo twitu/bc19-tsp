2c92e4af79a278ec9c310208f4a8a76490af50f2
Arkonaire <shantomborah@gmail.com>
2019-01-21 18:58:52 +0530

    Comments and castleTalk added.

--- bc19/Church.java
+++ bc19/Church.java
@@ -20,7 +20,7 @@ public class Church {
     // Private Variables
     LinkedList<Point> fuel_depots;
     LinkedList<Point> karb_depots;
-    boolean fuelCap, karbCap, combat, created;
+    boolean fuelCap, karbCap, combat;
     ArrayList<Integer> assigned_pilgrims = new ArrayList<>();
     ArrayList<Point> assigned_depots = new ArrayList<>();
     Point nextP;
@@ -42,23 +42,14 @@ public class Church {
         // Initialize church
         robo.log("I am at " + Integer.toString(me.x) + "," + Integer.toString(me.y));
         combat = false;
-        created = false;
         manager.updateData();
         
         // Record resource point locations
         Cluster D = resData.resourceList.get(resData.getID(me.x, me.y));
         fuel_depots = new LinkedList<>(D.fuelPos);
         karb_depots = new LinkedList<>(D.karboPos);
-        if (fuel_depots.size() == 0) {
-            fuelCap = true;
-        } else {
-            fuelCap = false;
-        }
-        if (karb_depots.size() == 0) {
-            karbCap = true;
-        } else {
-            karbCap = false;
-        }
+        fuelCap = (fuel_depots.size() == 0) ? true : false;
+        karbCap = (karb_depots.size() == 0) ? true : false;
 
         // Record depot of first pilgrim
         for(Robot r: manager.vis_robots){
@@ -100,6 +91,8 @@ public class Church {
         // Check for enemies and broadcast if under attack
         boolean noCombat = true;
         for (Robot bot: manager.vis_robots){
+            
+            // Enemy in sight?
             if (bot.team != me.team) {
                 noCombat = false;
                 if (!combat) {
@@ -108,6 +101,8 @@ public class Church {
                 }
                 break;
             }
+
+            // Emergency broadcast from nearby unit?
             if ((bot.team == me.team) && (bot.signal % 16 == 5)) {
                 Point enemy_loc = new Point(-1, -1);
                 enemy_loc.x = bot.signal/1024;
@@ -120,8 +115,13 @@ public class Church {
                 break;
             }
         }
+
+        // Defense successful? Then go to normal mode but be alert. Also tell castle
         if (noCombat) {
             combat = false;
+            robo.castleTalk(1);
+        } else {
+            robo.castleTalk(2);
         }
 
         // TODO: Pilgrim Tacking: Keep track of pilgrims
@@ -144,26 +144,34 @@ public class Church {
         if(manager.buildable(robo.SPECS.PILGRIM)) {
             Point p = manager.findEmptyAdj(me, true);
             if(p != null){
+                
+                // More karbonite to be mined
                 if ((karbCap) && (!fuelCap)) {
                     nextP = fuel_depots.pollFirst();                                        
                     if(fuel_depots.size()==0){
                         fuelCap = true;
                     }
+
+                // Full capacity karbonite production. Mine fuel
                 } else if (!karbCap){
                     nextP = karb_depots.pollFirst();
                     if(karb_depots.size()==0){
                         karbCap = true;
                     }
+
+                // Base producing at max capacity. Sit tight
                 } else {
-                    // TODO: Castle Correspondence
                     return null;
                 }
+
+                // Send the broadcast and build the unit
                 robo.signal(robo.radio.assignDepot(nextP), 2);
                 assigned_depots.add(nextP);
-                // TODO: Pilgrim Tracking: created = true;
                 return robo.buildUnit(robo.SPECS.PILGRIM, p.x, p.y);
             }
         }
+
+        // If confused, sit tight
         return null;
     }
 
